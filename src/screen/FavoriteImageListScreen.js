6838708ada8325158446b3e6618f2edef50b8dab
Kun Woo Kim <kimkuns98@gmail.com>
2023-09-13 10:49:22 +0900

    현재 redux만 설치됨. 앱 재실행시 store의 상태를 지속적으로 저장하기 위해 redux-persist를 설치할 예정

--- src/screen/FavoriteImageListScreen.js
+++ src/screen/FavoriteImageListScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import { Typography } from '../components/Typography';
+import { Header } from '../components/Header/Header';
+import { PhotoListItem } from '../components/PhotoListItem';
 
 export const FavoriteImageListScreen = (props)=>{
+    const imageList = useSelector((state)=> state.favorite.favoriteList);
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Typography fontSize={ 20 }>Favorite IMAGE</Typography>
+        <View style={{flex:1}}>
+            <Header>
+                <Header.Title title='FAVORITE'></Header.Title>
+            </Header>
+            <FlatList
+                style={{flex:1}}
+                data={imageList}
+                renderItem={({item})=>{
+                    return (
+                        <PhotoListItem url={item} />
+                    )
+                }}
+            />
         </View>
     )
 }
--- src/screen/ImageListScreen.js
+++ src/screen/ImageListScreen.js
@@ -1,6 +1,6 @@
 import React, { FlatList } from 'react-native';
-import {View} from 'react-native';
-import {Typography} from '../components/Typography';
+import { View } from 'react-native';
+import { Typography } from '../components/Typography';
 import { Header } from '../components/Header/Header';
 import { IMAGE_LIST } from '../constants';
 import { PhotoListItem } from '../components/PhotoListItem';
@@ -8,21 +8,21 @@ import { PhotoListItem } from '../components/PhotoListItem';
 export const ImageListScreen = (props)=>{
 
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{flex:1}}>
             <Header>
                 <Header.Group>
                     <Header.Title title='IMAGE LIST'/>
                 </Header.Group>
             </Header>
-            <FlatList 
-                style={{ flex: 1 }}
-                data={ IMAGE_LIST }
+            <FlatList
+                style={{flex:1}}
+                data={IMAGE_LIST}
                 renderItem={({item})=>{
-                    return(
-                        <PhotoListItem url={item} />
+                    return (
+                        <PhotoListItem url={item}/>
                     )
                 }}
-            ></FlatList>
+            />
         </View>
     )
 }
